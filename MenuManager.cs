bdb8af743a08d7803cac0a40ba9fddba3c93f220
cyriu77 <lagmon.1981@mail.ru>
2025-01-31 03:55:54 +0500

    Create MenuManager.cs

---
+++ MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MenuManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    public InputField createInput; 
+    public InputField joinInput; 
+    // Start is called before the first frame update
+    public void CreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 3;
+        PhotonNetwork.CreateRoom(createInput.text, roomOptions);
+    }
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+}
